1913aa30aa439e1348c973253c8b2662b9564b14
Jon Estrada <jonathan.estrada747@gmail.com>
2021-01-06 21:18:09 -0800

    created index.php for production

---
+++ index_old.php
@@ -0,0 +1,266 @@
+<?php
+//    require_once "/home/ucicslcu/public_html/nexus/private/includes/header.php";
+
+    //Redirect if not set
+/*
+    if (!isset($_GET["old"])) {
+        header('Location: '."https://sites.google.com/uci.edu/ctglv4dlc/");
+        die;
+    }
+ */
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>CTG v6: DLC Edition</title>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap" rel="stylesheet">
+        <link rel="shortcut icon" type="image/png" href="img/ctgsmall.png">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <head> 
+        <body>
+            <div id="menu">
+                <div id="menu_left">
+                <a class="active" >Change Thru Games v6: DLC Edition</a>
+                </div>
+                <div id="menu_right">
+                    <a href="#home">Home</a>
+                    <a href="#donate">Donate</a>
+                    <a href="#commitee">Committee</a>
+                    <a href="https://www.twitch.tv/changethrugames/">Watch Here!</a>
+                </div>
+            </div>
+            
+            <div id="mainpic">
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <img src="img\ctg cover.png" alt="CTG Cover" class="center" >
+
+            </div>
+
+            <section id= "home">
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            
+
+                <h6 style="font-size: 60px"><center>What is Change Thru Games?</center></h6>
+                <h6  style="font-size: 30px"><center>Change Thru Games v6: DLC Edition is an 8-hour video game marathon brought to you by UCI Circle K!</center><h6>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <div class="box-container3">
+                    <div id="pic2" class="clearbox2">
+                        <p>&nbsp;</p>
+                        <a target="_blank"><img src="img\control.png" style="width: 100px; height: 100px"></a>
+                        <h4> Change Thru Games offers will host gaming tournaments for players to participate in this year with games such as League of Legends, Tetris, and more! </h4>
+                    </div>
+
+                    <div id="ctg" class="clearbox2">
+                        <a target="_blank"><img src="img\card.png" style="width: 100px; height: 100px"></a>
+                        <p>&nbsp;</p>
+                        <h4>Alongside video games at Change Thru Games, players will also have the opportunity to be apart of board game tournaments such as Codenames, Mafia, and Egyptian War! </h4>
+                    </div>
+
+                    <div id="pic3" class="clearbox2">
+                        <p>&nbsp;</p>
+                        <a target="_blank" href="https://docs.google.com/document/d/1lNxOQcTnk0gU1SqVscKfuImfx8v9QJwCMwj7ji_QD_U/edit?usp=sharing"><img src="img\donation.png" alt="Here is a link to our panphlet!" style="width: 100px; height: 100px"></a>
+                        <h4>Every year all of the proceeds earned at Change Thru Games will be donated to a charity! To read more about it, click on the image above to read our sponsorship pamphlet! </h4>
+                    </div>
+
+                </div>
+            </section>
+
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+
+
+            <div class="box-container2">
+                <div id="pic1" class="clearbox">
+                    <a target="_blank" ><img src="img\ckibackground.png"  style="width: 600px; height: 279px; border-radius: 15px;"></a>
+                </div>
+                <div id="ctg" class="clearbox">
+                    <h3>About UCI Circle K</h3>
+                    <h4>Circle K clubs all over the world are based on the tenets of Service, Leadership, and Fellowship. 
+                        We are the largest collegiate student-led service organization in the world, and UCI boasts the biggest club in all of Circle K International. 
+                        From weekly service projects and leadership events to a welcoming family system and growing mentor-mentee program, there is something for everyone here in CKI, 
+                        and we hope you enjoy your time with us from the moment you join! Give more, do more, be more. That's what it means to be part of UC Irvine Circle K!
+                    </h4>
+                    <p>&nbsp;</p>
+                    <h4>To see our website, please press on the link below: 
+                    <a target="_blank" href="http://www.ucicirclek.com/">http://www.ucicirclek.com/</a>
+                        </h4>
+                </div>
+            </div>
+            
+            <section id="donate"></section>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+
+                <div class="box-container">
+                    <div id="fundraiser" class="curvedbox">
+                        <h3>Supporting Covid-19 Relief</h3>
+                        <h4>This year we are donating all of our proceeds to the CDP Covid-19 Response Fund. The CDP COVID-19 Response Fund is 
+                            focusing on support for nonprofit organizations working directly to respond to the pandemic among vulnerable populations 
+                            in order to help build their capacity for response. These include social service organizations focused 
+                            on supporting the following:</h4>
+                            <p>&nbsp;</p>
+                        <ul style="font-size:16px">
+                            <li>- low-income households - small businessess&nbsp;</li>
+                            <li>- immigrant/New American populations&nbsp;</li>
+                            <li>- people with disabilities</li>
+                            <li>- communities vulnerable to the physical health, mental health and economic impacts of the pandemic.</li>
+                        </ul>
+                    </div>
+
+                        <div id="sponsors" class="curvedbox">
+                            <h3>How To Donate</h3>
+                            <h4>To donate to the cause, press one of the pictures, use one of the following methods below, and comment Change Thru Games!</h4>
+                            <p>&nbsp;</p>
+                            <a target="_blank" href="https://venmo.com/ucicki"><img src="img\venmo.png" alt="Donate through Venmo here!" style="width: 50px; height: 50px"></a>
+                            &nbsp; &nbsp; &nbsp; &nbsp; 
+                            <a target="_blank" href="https://www.paypal.me/dbanana01"><img src="img\paypal.png" alt="Donate through Paypal here!" style="width: 50px; height: 50px"></a>
+                            <h3>And a big thank you to our sponsors!</h3>
+                            <p>&nbsp;</p>
+                            <a target="_blank" href="https://www.turtlebeach.com/"><img src="img\turtle.png" style="width: 90px; height: 56px"></a>
+                            <a target="_blank" href="https://www.goldenguardians.com/"><img src="img\ggs.png" style="width: 47px; height: 65px"></a>
+                            &nbsp; &nbsp;
+                            <a target="_blank" href="https://www.goldenguardians.com/"><img src="img\uciesports.png" style="width: 60px; height: 60px"></a>
+                        </div>
+                    </div>
+            </div>
+
+            <section id="commitee"></section>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+
+            <div id='commitee'>
+                <h3><center>Meet our Change Thru Games Commitee!</center></h3> <p>&nbsp;</p>
+                <center>
+                    <figure>
+                        <img src="img\james.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>James Molamphy</p><p style="font-size: 12px">CTG Director</p></figcaption><p style="font-size: 12px">&nbsp;</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\emily.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Emily Yan</p><p style="font-size: 12px">Admin. Production</p><p style="font-size: 12px">Assistant</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\jeff.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Jeff Moreno</p><p style="font-size: 12px">Director's</p></figcaption><p style="font-size: 12px">Assistant</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\kenny.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Kenneth Seng</p><p style="font-size: 12px">Communications</p><p style="font-size: 12px">Chair</p></figcaption>
+                    </figure>
+
+                    <figure>
+                        <img src="img\nat.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Natashza Trejo</p><p style="font-size: 12px">Marketing Chair</p><p style="font-size: 12px">&nbsp;</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\bryant.png" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Bryant Ly</p><p style="font-size: 12px">Programs Chair</p><p style="font-size: 12px">&nbsp;</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\chaya.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Chaya Pearl</p><p style="font-size: 12px">Fundraising</p><p style="font-size: 12px">Chair</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\brenda.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Brenda Wong</p><p style="font-size: 12px">Creative Production</p><p style="font-size: 12px">Assistant</p></figcaption>
+                    </figure>
+                    <figure>
+                        <img src="img\brian.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption><p>Brian Nguyen</p><p style="font-size: 12px">Sponsorship Chair</p><p style="font-size: 12px">&nbsp;</p></figcaption>
+                    </figure>
+                </center>
+            </div>
+
+            <p>&nbsp;</p>
+
+            <div id='streamteam'>
+                <h3><center>Meet our Change Thru Games Streaming Team!</center></h3> <p>&nbsp;</p>
+                <center>
+                    <figure>
+                        <img src="img\brandon.png" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption>Brandon Kincaid</figcaption>
+                    </figure>
+
+                    <figure>
+                        <img src="img\arthur.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption>Arthur Tham</figcaption>
+                    </figure>
+    
+                    <figure>
+                        <img src="img\hayden.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption>Hayden Poulain</figcaption>
+                    </figure>
+    
+                    <figure>
+                        <img src="img\rin.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption>Rin Nguyen</figcaption>
+                    </figure>
+                    
+                    <figure>
+                        <img src="img\terry.jpg" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption>Terry Nguyen</figcaption>
+                    </figure>           
+                    <figure>
+                        <img src="img\tin.png" style="width: 100px; height: 100px; border-radius: 15px;">
+                        <figcaption>Tin Luu</figcaption>
+                    </figure>
+                    
+                    
+                    </center>
+            </div>
+
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            
+
+
+            <section id="footer">
+            <div id="logos">
+                </div>
+                <hr class="featurette-divider">
+                <div id="emblems" style="float:right; position: relative; top:-50px;">
+                <a target="_blank" href="https://www.facebook.com/groups/cnhckicitrus"><img src="img\citrus_emblem.png" alt="Citrus Division" style="width: 100px; height: 100px"></a>
+                <a target="_blank" href="http://www.cnhcirclek.org/"><img src="img\district_emblem.png" alt="Cal-Nev-Ha District" style="width: 100px; height: 100px"></a>
+                <a target="_blank" href="http://www.circlek.org/"><img src="http://www.ucicirclek.com/public/css/cki_emblem.png" alt="Circle K International" style="width: 100px; height: 100px"></a>
+                </div>
+                <span style="font-family: 'Open Sans', sans-serif; color: white; text-align: center;">
+                    (c) 2020 University of California, Irvine - Circle K - All Rights Reserved.
+                    <br>You may not distribute, copy, nor sell this content without explicit permission from its owner.<br>
+                </span>
+            </footer>
+
+
+        </body>
+    </head>
+</html>
+
+
+
+
