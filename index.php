43c9da5443db0c6fee51176e5eb8e7ff49eb974c
Jon Estrada <jonathan.estrada747@gmail.com>
2021-01-09 14:00:58 -0800

    updated index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
         <title>Change Thru Games 7</title>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap" rel="stylesheet">
         <link rel="shortcut icon" type="image/png" href="https://i.imgur.com/Z34ZWtw.png">
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <link href="index.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <meta charset="UTF-8">
@@ -24,6 +25,14 @@
 
     <head> 
         <body>
+
+            <!-- Animate on Scroll library -->
+            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+            <script>
+                AOS.init();
+            </script>
+
+
             <div id="menu">
                 <div id="menu_left">
                 <a class="active" >INSERT LOGO HERE</a>
@@ -66,6 +75,99 @@
                 <p>&nbsp;</p>
                 <p>&nbsp;</p>
             </section>
+
+            <div class="box-container3" style="margin-top: 5%;">
+                <div id="pic2" class="clearbox2" data-aos="flip-left" data-aos-duration="800">
+                    <a target="_blank"><img src="https://i.imgur.com/f7Fifz9.png" style="width: 100px; height: 100px"></a>
+                    <p>&nbsp;</p>
+                    <h4> Change Thru Games offers will host gaming tournaments for players to participate in this year with games such as League of Legends, Tetris, and more! </h4>
+                </div>
+                <div id="ctg" class="clearbox2" data-aos="flip-up" data-aos-duration="800">
+                    <a target="_blank"><img src="https://i.imgur.com/S5EKse4.png" style="width: 100px; height: 100px"></a>
+                    <p>&nbsp;</p>
+                    <h4>Alongside video games at Change Thru Games, players will also have the oppurtunity to be apart of board game tournaments such as Codenames, Mafia, and Egyptian War! </h4>
+                </div>
+
+                <div id="pic3" class="clearbox2" data-aos="flip-right" data-aos-duration="800">
+                    <a target="_blank" href="https://docs.google.com/document/d/1lNxOQcTnk0gU1SqVscKfuImfx8v9QJwCMwj7ji_QD_U/edit?usp=sharing"><img src="https://i.imgur.com/3ebGk9v.png" alt="Here is a link to our panphlet!" style="width: 100px; height: 100px"></a>
+                    <p>&nbsp;</p>
+                    <h4>Every year all of the proceeds earned at Change Thru Games will be donated to a charity! To read more about it, click on the image above to read our sponsorship panphlet! </h4>
+                </div>
+
+            </div>
+
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+
+
+            <div class="box-container2">
+                <div id="pic1" class="clearbox" data-aos="fade-right" data-aos-duration="800">
+                    <a target="_blank" ><img src="https://i.imgur.com/FrOLIsa.png"  style="width: 600px; height: 279px; border-radius: 25px; box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.5);"></a>
+                </div>
+                <div id="ctg" class="clearbox" data-aos="fade-left" data-aos-duration="800">
+                    <h3>About UCI Circle K</h3>
+                    <p>&nbsp;</p>
+                    <h4>Circle K clubs all over the world are based on the tenets of Service, Leadership, and Fellowship. 
+                        We are the largest collegiate student-led service organization in the world, and UCI boasts the biggest club in all of Circle K International. 
+                        From weekly service projects and leadership events to a welcoming family system and growing mentor-mentee program, there is something for everyone here in CKI, 
+                        and we hope you enjoy your time with us from the moment you join! Give more, do more, be more. That's what it means to be part of UC Irvine Circle K!
+                    </h4>
+                    <p>&nbsp;</p>
+                    <h4>To visit our website, please press on the link below: 
+                    <a target="_blank" href="http://www.ucicirclek.com/">http://www.ucicirclek.com/</a>
+                        </h4>
+                </div>
+            </div>
+
+            <div class="box-container2">
+                <div id="ctg" class="clearbox" data-aos="fade-right" data-aos-duration="800" style="margin-left: 6%; ">
+                    <h3>Fundraising for Breast Cancer Support</h3>
+                    <p>&nbsp;</p>
+                    <h4>This year we are donating all of our proceeds to the the Breast Cancer Fundraiser. The Breast Cancer Fundraiser is an
+                        organization that supports breast cancer patients with value added services. In their services, they include "WeCare" packages
+                        that service breast cancer patients through their treatment and recovery, which focus on
+                        beauty, comfort, wellness, journey, and loveones. They are also work with corporate partners across the country 
+                        to host employee engagement events where they get to build packages and personalized notes for future patients in need.
+                        In addition to their packages, they also partner with hospitals to bring breast cancer patients together 
+                        for an afternoon of community, education, and the distrubution of their packages.
+
+                    </h4>
+                </div>
+
+                <div id="pic1" class="clearbox" data-aos="fade-left" data-aos-duration="800" style="margin-left: 0%;">
+                    <a target="_blank" ><img src="https://i.imgur.com/FrOLIsa.png"  style="width: 600px; height: 279px; border-radius: 25px; box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.5);"></a>
+                </div>
+            </div>
+            
+            
+            <section id="donate">
+                <center>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <p>&nbsp;</p>
+                <div id="sponsors" class="curvedbox" data-aos="fade-up" data-aos-duration="600">
+                    <h3>How To Donate</h3>
+                    <h4>To donate to the cause, press one of the pictures, use one of the following methods below, and comment Change Thru Games!</h4>
+                    <p>&nbsp;</p>
+                    <a target="_blank" href="https://venmo.com/ucicki"><img src="https://i.imgur.com/nuvzpHg.png" alt="Donate through Venmo here!" style="width: 50px; height: 50px"></a>
+                    &nbsp; &nbsp; &nbsp; &nbsp; 
+                    <a target="_blank" href="https://www.paypal.me/dbanana01"><img src="https://i.imgur.com/i7rZgRF.png" alt="Donate through Paypal here!" style="width: 50px; height: 50px"></a>
+                    <h3>And a big thank you to our sponsors!</h3>
+                    <p>&nbsp;</p>
+                    <a target="_blank" href="https://www.turtlebeach.com/"><img src="https://i.imgur.com/CWwESDR.png" style="width: 90px; height: 56px"></a>
+                    <a target="_blank" href="https://www.goldenguardians.com/"><img src="https://i.imgur.com/flaurNw.png" style="width: 47px; height: 65px"></a>
+                    &nbsp; &nbsp;
+                    <a target="_blank" href="https://esports.uci.edu/"><img src="https://i.imgur.com/dG7J9ic.png" style="width: 60px; height: 60px"></a>
+                    <a target="_blank" href="https://tohru.io/#/mainPage"><img src="https://i.imgur.com/KKKvZuV.png" style="width: 60px; height: 60px"></a>
+                </div>
+                </center>
+            </section>
+
+            <section id="commitee"></section>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
 
             <section id="tournaments">
                 <div class="tabs">
@@ -114,96 +216,7 @@
                     </div>
                 <script src=tab_script.js></script>
             </section>
-
-            <div class="box-container3">
-                <div id="pic2" class="clearbox2">
-                    <a target="_blank"><img src="https://i.imgur.com/f7Fifz9.png" style="width: 100px; height: 100px"></a>
-                    <p>&nbsp;</p>
-                    <h4> Change Thru Games offers will host gaming tournaments for players to participate in this year with games such as League of Legends, Tetris, and more! </h4>
-                </div>
-                <div id="ctg" class="clearbox2">
-                    <a target="_blank"><img src="https://i.imgur.com/S5EKse4.png" style="width: 100px; height: 100px"></a>
-                    <p>&nbsp;</p>
-                    <h4>Alongside video games at Change Thru Games, players will also have the oppurtunity to be apart of board game tournaments such as Codenames, Mafia, and Egyptian War! </h4>
-                </div>
-
-                <div id="pic3" class="clearbox2">
-                    <a target="_blank" href="https://docs.google.com/document/d/1lNxOQcTnk0gU1SqVscKfuImfx8v9QJwCMwj7ji_QD_U/edit?usp=sharing"><img src="https://i.imgur.com/3ebGk9v.png" alt="Here is a link to our panphlet!" style="width: 100px; height: 100px"></a>
-                    <p>&nbsp;</p>
-                    <h4>Every year all of the proceeds earned at Change Thru Games will be donated to a charity! To read more about it, click on the image above to read our sponsorship panphlet! </h4>
-                </div>
-
-            </div>
-
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-
-
-            <div class="box-container2">
-                <div id="pic1" class="clearbox">
-                    <a target="_blank" ><img src="https://i.imgur.com/FrOLIsa.png"  style="width: 600px; height: 279px; border-radius: 25px; box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.5);"></a>
-                </div>
-                <div id="ctg" class="clearbox">
-                    <h3>About UCI Circle K</h3>
-                    <p>&nbsp;</p>
-                    <h4>Circle K clubs all over the world are based on the tenets of Service, Leadership, and Fellowship. 
-                        We are the largest collegiate student-led service organization in the world, and UCI boasts the biggest club in all of Circle K International. 
-                        From weekly service projects and leadership events to a welcoming family system and growing mentor-mentee program, there is something for everyone here in CKI, 
-                        and we hope you enjoy your time with us from the moment you join! Give more, do more, be more. That's what it means to be part of UC Irvine Circle K!
-                    </h4>
-                    <p>&nbsp;</p>
-                    <h4>To visit our website, please press on the link below: 
-                    <a target="_blank" href="http://www.ucicirclek.com/">http://www.ucicirclek.com/</a>
-                        </h4>
-                </div>
-            </div>
-
-            <div class="box-container2">
-                <div id="ctg" class="clearbox">
-                    <h3>Fundraising for Breast Cancer Support</h3>
-                    <p>&nbsp;</p>
-                    <h4>This year we are donating all of our proceeds to the the Breast Cancer Fundraiser. The Breast Cancer Fundraiser is an
-                        organization that supports breast cancer patients with value added services. In their services, they include "WeCare" packages
-                        that service breast cancer patients through their treatment and recovery, which focus on
-                        beauty, comfort, wellness, journey, and loveones. They are also work with corporate partners across the country 
-                        to host employee engagement events where they get to build packages and personalized notes for future patients in need.
-                        In addition to their packages, they also partner with hospitals to bring breast cancer patients together 
-                        for an afternoon of community, education, and the distrubution of their packages.
-
-                    </h4>
-                </div>
-
-                <div id="pic1" class="clearbox">
-                    <a target="_blank" ><img src="https://i.imgur.com/FrOLIsa.png"  style="width: 600px; height: 279px; border-radius: 25px; box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.5);"></a>
-                </div>
-            </div>
             
-            
-            <section id="donate">
-                <center>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <div id="sponsors" class="curvedbox">
-                    <h3>How To Donate</h3>
-                    <h4>To donate to the cause, press one of the pictures, use one of the following methods below, and comment Change Thru Games!</h4>
-                    <p>&nbsp;</p>
-                    <a target="_blank" href="https://venmo.com/ucicki"><img src="https://i.imgur.com/nuvzpHg.png" alt="Donate through Venmo here!" style="width: 50px; height: 50px"></a>
-                    &nbsp; &nbsp; &nbsp; &nbsp; 
-                    <a target="_blank" href="https://www.paypal.me/dbanana01"><img src="https://i.imgur.com/i7rZgRF.png" alt="Donate through Paypal here!" style="width: 50px; height: 50px"></a>
-                    <h3>And a big thank you to our sponsors!</h3>
-                    <p>&nbsp;</p>
-                    <a target="_blank" href="https://www.turtlebeach.com/"><img src="https://i.imgur.com/CWwESDR.png" style="width: 90px; height: 56px"></a>
-                    <a target="_blank" href="https://www.goldenguardians.com/"><img src="https://i.imgur.com/flaurNw.png" style="width: 47px; height: 65px"></a>
-                    &nbsp; &nbsp;
-                    <a target="_blank" href="https://esports.uci.edu/"><img src="https://i.imgur.com/dG7J9ic.png" style="width: 60px; height: 60px"></a>
-                    <a target="_blank" href="https://tohru.io/#/mainPage"><img src="https://i.imgur.com/KKKvZuV.png" style="width: 60px; height: 60px"></a>
-                </div>
-                </center>
-            </section>
-
-            <section id="commitee"></section>
             <p>&nbsp;</p>
             <p>&nbsp;</p>
             <p>&nbsp;</p>
