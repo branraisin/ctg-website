3bdb097448d315c1a290e585c990d51d07b8fbc6
branraisin <branraisin@users.noreply.github.com>
2021-01-13 00:53:04 -0800

    Converted images to imgur
    Alot of minor details fixed

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <head>
         <title>Change Thru Games 7</title>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap" rel="stylesheet">
-        <link rel="shortcut icon" type="image/png" href="https://i.imgur.com/Z34ZWtw.png">
+        <link rel="shortcut icon" type="image/png" href="https://i.imgur.com/iisZaXN.png">
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <link href="index.css" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -35,7 +35,7 @@
 
             <div id="menu">
                 <div id="menu_left">
-                <a class="active" >INSERT LOGO HERE</a>
+                <a class="active" >Change Thru Games 7</a>
                 </div>
                 <div id="menu_right">
                     <a href="#home">Home</a>
@@ -95,10 +95,9 @@
             <p>&nbsp;</p>
             <p>&nbsp;</p>
             <p>&nbsp;</p>
-
-
+            
             <div class="box-container2">
-                <div id="pic1" class="clearbox" data-aos="zoom-in" data-aos-duration="800" style="margin-left: -1%;">
+                <div id="pic1" class="clearbox" data-aos="zoom-in" data-aos-duration="800">
                     <a target="_blank" ><img src="https://i.imgur.com/FrOLIsa.png"  style="width: 600px; height: 279px; border-radius: 25px; box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.5);"></a>
                 </div>
                 <div id="ctg" class="clearbox" data-aos="zoom-in" data-aos-duration="800">
@@ -117,10 +116,11 @@
             </div>
 
             <div class="box-container2">
-                <div id="ctg" class="clearbox" data-aos="zoom-in" data-aos-duration="800" style="margin-left: 6%; ">
+                <div id="ctg" class="clearbox" data-aos="zoom-in" data-aos-duration="800">
                     <h3>Fundraising for Breast Cancer Support</h3>
                     <p>&nbsp;</p>
-                    <h4>This year we are donating all of our proceeds to the Breast Cancer Fundraiser. The Breast Cancer Fundraiser is an
+                    <h4>
+                        This year we are donating all of our proceeds to the Breast Cancer Fundraiser. The Breast Cancer Fundraiser is an
                         organization that supports breast cancer patients with value added services. In their services, they include "WeCare" packages
                         that service breast cancer patients through their treatment and recovery, which focus on
                         beauty, comfort, wellness, journey, and loved ones. They are also work with corporate partners across the country 
@@ -130,12 +130,11 @@
                     </h4>
                 </div>
 
-                <div id="pic1" class="clearbox" data-aos="zoom-in" data-aos-duration="800" style="margin-left: 0%; margin-right: 6%;">
+                <div id="pic1" class="clearbox" data-aos="zoom-in" data-aos-duration="800">
                     <a href="https://www.thebreastcancerfundraiser.org/" target="_blank" ><img src="https://i.imgur.com/nEr4TOs.png" style="width: 600px; height: 279px; border-radius: 25px; box-shadow: 5px 5px 10px 10px rgba(0,0,0,0.5);"></a>
                 </div>
             </div>
-            
-            <!-- maybe we can change to fade-up ????-->
+
             <div id="donate" data-aos="zoom-in" data-aos-duration="800">
                 <center>
                 <p>&nbsp;</p>
@@ -155,15 +154,16 @@
                     <p>&nbsp;</p>
                     <p>&nbsp;</p>
                     <a target="_blank" href="https://bouncealerts.com/"><img src="https://i.imgur.com/UY2x3We.png" style="width: 100; height: 100px"></a>
-                    <a target="_blank" href="https://www.goldenguardians.com/"><img src="https://i.imgur.com/flaurNw.png" style="width: 72px; height: 100px"></a>
-                    &nbsp; &nbsp;
+                    &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+                    <a target="_blank" href="https://glassbottomgames.com/"><img src="https://i.imgur.com/we2UW1a.png" style="width: 53px; height: 100px"></a>
+                    &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
                     <a target="_blank" href="https://esports.uci.edu/"><img src="https://i.imgur.com/dG7J9ic.png" style="width: 100px; height: 100px"></a>
+                    &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
                     <a target="_blank" href="https://tohru.io/#/mainPage"><img src="https://i.imgur.com/KKKvZuV.png" style="width: 100px; height: 100px"></a>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
                 </center>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
-                <p>&nbsp;</p>
             </div>
 
 
@@ -188,7 +188,7 @@
                         
                             <div class="ctg-tab active">
                                 <div style="margin-left: 0%; margin-bottom: 3%;">
-                                    <img class="ctg-img" src="./img/James.jpg" alt="James" style="width: 120px; height: 120px;"></img>
+                                    <img class="ctg-img" src="https://i.imgur.com/X8lSVbA.jpg" alt="James" style="width: 120px; height: 120px;"></img>
                                     <h2 style="margin-bottom: 0;">James Molamphy</h2>
                                     <h4>CTG Director</h4>
                                 </div>
@@ -200,7 +200,7 @@
                                         <h4 style="width: 10em;">Competitive Production Assistant</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/Chaya.jpg" alt="Chaya" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/VzxLjzJ.jpg" alt="Chaya" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Chaya Pearl</h2>
                                         <h4 style="width: 10em;">Administrative Production Assistant</h4>
@@ -214,7 +214,7 @@
                                         <h4 style="width: 10em;">Creative Production Assistant</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/Mercy.jpg" alt="Mercy" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/QdOP7c4.jpg" alt="Mercy" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2 class="h2-name">Mercy Anne Cruz</h2>
                                         <h4 style="width: 10em;">Fundraising Chair</h4>
@@ -236,13 +236,13 @@
                                 </div>
     
                                 <div class="ctg-row">
-                                    <img class="ctg-img-left" src="./img/Brandon.jpg" alt="Brandon" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-left" src="https://i.imgur.com/qZU0SgJ.jpg" alt="Brandon" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Brandon Kincaid</h2>
                                         <h4 style="width: 10em;">Marketing Chair</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/CJ.jpg" alt="CJ" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/RfVQ2Tt.png" alt="CJ" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2 class="h2-name">CJ Cabrera</h2>
                                         <h4 style="width: 10em;">Stream Chair</h4>
@@ -252,13 +252,13 @@
                             
                             <div class="ctg-tab">  
                                 <div class="ctg-row">
-                                    <img class="ctg-img-left" src="./img/Celina.jpg" alt="Celina" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-left" src="https://i.imgur.com/wn4TpRb.jpg" alt="Celina" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Celina Tiqui</h2>
                                         <h4 style="width: 10em;">Administrative Intern</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/Izzabella.jpg" alt="Izzabella" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/QCzizds.jpg" alt="Izzabella" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Izzabella Ramiscal</h2>
                                         <h4 style="width: 10em;">Administrative Intern</h4>
@@ -266,13 +266,13 @@
                                 </div>
     
                                 <div class="ctg-row">
-                                    <img class="ctg-img-left" src="./img/Jon.jpg" alt="Jon" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-left" src="https://i.imgur.com/iZH5SIT.jpg" alt="Jon" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Jon Estrada</h2>
                                         <h4 style="width: 10em;">Administrative Intern</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/Valerie.jpg" alt="Valerie" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/LH80YK0.jpg" alt="Valerie" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Valerie Nguyen</h2>
                                         <h4 style="width: 10em;">Administrative Intern</h4>
@@ -288,7 +288,7 @@
                                         <h4 style="width: 10em;">Competitive Intern</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/Jen.jpg" alt="Jen" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/yhVtSg7.jpg" alt="Jen" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Jen Delgado</h2>
                                         <h4 style="width: 10em;">Competitive Intern</h4>
@@ -296,7 +296,7 @@
                                 </div>
     
                                 <div class="ctg-row">
-                                    <img class="ctg-img-left" src="./img/Shania.jpg" alt="Shania" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-left" src="https://i.imgur.com/UBsoR35.jpg" alt="Shania" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Shania Relucio</h2>
                                         <h4 style="width: 10em;">Competitive Intern</h4>
@@ -332,7 +332,7 @@
                                         <h4 style="width: 10em;">Creative Intern</h4>
                                     </div>
     
-                                    <img class="ctg-img-right" src="./img/Ian.jpg" alt="Ian" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-right" src="https://i.imgur.com/n3ejbDv.jpg" alt="Ian" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 2%;">
                                         <h2>Ian Baylon</h2>
                                         <h4 style="width: 10em;">Creative Intern</h4>
@@ -340,7 +340,7 @@
                                 </div>  
                                 
                                 <div class="ctg-row">
-                                    <img class="ctg-img-left" src="./img/Jay.jpg" alt="Jay" style="margin-right: 20px;"></img>
+                                    <img class="ctg-img-left" src="https://i.imgur.com/pOlr0EY.jpg" alt="Jay" style="margin-right: 20px;"></img>
                                     <div style="margin-top: 4%;">
                                         <h2>Jay Han</h2>
                                         <h4 style="width: 10em;">Creative Intern</h4>
